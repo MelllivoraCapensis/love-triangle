16c6b3ea7dc4d271958414cf85134c6dafb52487
MelllivoraCapensis <kobzarevdmitry@yandex.ru>
2018-09-13 20:13:29 +0300

    done

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count=0;
+  for(var i=1;i<=preferences.length;i++)
+  {
+  	if(preferences[i-1]===i)
+  		continue;
+  	else 
+  		if(preferences[preferences[preferences[i-1]-1]-1]===i)
+  	    count++;
+  }
+  return count/3;
 };
